ca1d5a159f083c2c52e37d7f11b88437507ee8ef
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-12-04 11:38:52 +0200

    init

--- StretchyHeaderUsingSwiftUI/ContentView.swift
+++ StretchyHeaderUsingSwiftUI/ContentView.swift
@@ -10,7 +10,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack {
+                GeometryReader { geo in
+                    Image("1")
+                        .resizable()
+                        .scaledToFit()
+                        .offset(y: geo.frame(in: .global).minY > 0 ? -geo.frame(in: .global).minY : 0)
+                        .frame(height: geo.frame(in: .global).minY > 0 ? 300 + geo.frame(in: .global).minY : 300)
+                    Spacer()
+                }.frame(height: 300)
+                
+                ForEach(0..<18) { _ in
+                    HStack(spacing: 15) {
+                        Image("2")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 45, height: 45)
+                            .clipShape(Circle())
+                        VStack {
+                            Text("Taylor Swift")
+                                .fontWeight(.heavy)
+                            Text("Love Story")
+                        }
+                        Spacer()
+                    }.padding(.horizontal, 15)
+                }
+            }
+        }
     }
 }
 
